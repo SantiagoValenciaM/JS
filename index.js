bfe6e33c54e8464f23612715d3f803dc90d429cd
SantiagoValenciaM <svalencia7@ucol.mx>
2023-10-25 12:55:05 -0600

    Updated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let nombre = prompt("Escribe tu nombre")
-let apellido = prompt("Escribe un apellido")
 let edad = prompt("Escribe tu edad");
 
 if (edad >= 18) {
+    let nombre = prompt("Escribe tu nombre")
+    let apellido = prompt("Escribe un apellido")
     document.write(`Hola, ${nombre} ${apellido}`);
 } else {
     document.write("Lo siento no eres mayor de edad");
